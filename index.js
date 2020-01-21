91aa7bc1c8174f48a3f7eb5aa688fe6bb7d24996
sprajjwal <shashwatprajjwal@gmail.com>
2020-01-21 12:12:07 -0800

    moving ball

--- index.js
+++ index.js
@@ -2,52 +2,57 @@
     const canvas = document.getElementById("myCanvas")
     const ctx = canvas.getContext("2d")
 
-    const margin = 10;
-    let x = margin;
-    let y = margin;
+    // const margin = 10;
+    // let x = margin;
+    // let y = margin;
     
 
-    console.log(canvas.style.width)
-    function drawBricks(width, height) {
-        // console.log(x)
+    // console.log(canvas.style.width)
+    // function drawBricks(width, height) {
+    //     // console.log(x)
+    //     ctx.beginPath();
+    //     ctx.rect(x, y, width, height);
+    //     ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    //     ctx.fill();
+    //     ctx.closePath();
+    // }
+
+    // function makeRow(width, height) {
+    //     while (x + margin + width <= 470 ) {
+    //         drawBricks( width, height)
+    //         x += width + margin
+    //     }
+    //     console.log(y)
+    //     x = margin
+    //     y += height + margin;
+    //     console.log(y)
+    // }
+
+    // makeRow(30, 20)
+    // makeRow(70, 20)
+    // makeRow(30, 20)
+    // makeRow(70, 20)
+
+    let x = canvas.width/2;
+    let y = canvas.height-30;
+    let dx = 2;
+    let dy = -2;
+
+    function drawBall() {
         ctx.beginPath();
-        ctx.rect(x, y, width, height);
-        ctx.fillStyle = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+        ctx.arc(x, y, 10, 0, Math.PI*2);
+        ctx.fillStyle = "#0095DD";
         ctx.fill();
         ctx.closePath();
-
-        if ((x + width + margin) >= 480) {
-            x = margin
-            y +=  height + margin
-        } else {
-            x += width + margin
-        }
-
+    }
+    
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+        x += dx;
+        y += dy;
     }
 
-    let refreshID = setInterval(function() {
-        drawBricks(70, 20)
-        // drawBricks(140, 20)
-        if (y >= 150) {
-            clearInterval(refreshID)
-        }
-    }, 50)
+    setInterval(draw, 10);
 
-    // ctx.beginPath();
-    // ctx.rect(20, 40, 50, 50);
-    // ctx.fillStyle = "#FF0000";
-    // ctx.fill();
-    // ctx.closePath();
-
-    // ctx.beginPath();
-    // ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-    // ctx.fillStyle = "green";
-    // ctx.fill();
-    // ctx.closePath();
-
-    // ctx.beginPath();
-    // ctx.rect(160, 10, 100, 40);
-    // ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-    // ctx.stroke();
-    // ctx.closePath();
 })();
