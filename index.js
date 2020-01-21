7a115a0c767a13d9b3d0cf1d2a3a6ee080198a06
sprajjwal <shashwatprajjwal@gmail.com>
2020-01-21 12:30:41 -0800

    paddle added and moving

--- index.js
+++ index.js
@@ -33,11 +33,37 @@
     // makeRow(30, 20)
     // makeRow(70, 20)
 
+    const ballRadius = 10;
     let x = canvas.width/2;
     let y = canvas.height-30;
     let dx = 2;
     let dy = -2;
-    const ballRadius = 10
+    const paddleHeight = 10;
+    const paddleWidth = 75;
+    let paddleX = (canvas.width-paddleWidth)/2;
+    let rightPressed = false;
+    let leftPressed = false;
+
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+
+    function keyDownHandler(e) {
+        if(e.key == "Right" || e.key == "ArrowRight") {
+            rightPressed = true;
+        }
+        else if(e.key == "Left" || e.key == "ArrowLeft") {
+            leftPressed = true;
+        }
+    }
+
+    function keyUpHandler(e) {
+        if(e.key == "Right" || e.key == "ArrowRight") {
+            rightPressed = false;
+        }
+        else if(e.key == "Left" || e.key == "ArrowLeft") {
+            leftPressed = false;
+        }
+    }
 
     function drawBall() {
         ctx.beginPath();
@@ -46,10 +72,18 @@
         ctx.fill();
         ctx.closePath();
     }
-    
+    function drawPaddle() {
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
+        drawPaddle();
         
         if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
             dx = -dx;
@@ -58,10 +92,25 @@
             dy = -dy;
         }
         
+        if(rightPressed) {
+            paddleX += 7;
+            if (paddleX + paddleWidth > canvas.width){
+                paddleX = canvas.width - paddleWidth;
+            }
+        }
+        else if(leftPressed) {
+            paddleX -= 7;
+            if (paddleX < 0){
+                paddleX = 0;
+            }
+        }
+        
         x += dx;
         y += dy;
     }
-    
+
     setInterval(draw, 10);
+
+
 
 })();
